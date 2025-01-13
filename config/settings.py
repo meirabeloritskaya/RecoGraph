a66faa4c4ba4a904fa8c953d335c98a66adf7f0f
Liudmila Beloritskaya <meiramirth@gmail.com>
2025-01-13 23:56:58 +0200

    add user model

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'users',
+    'recipients',
+    'events',
+    'categories',
+    'items',
+    'recommendations',
+    'analytics'
 ]
 
 MIDDLEWARE = [
--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+# Кастомная модель пользователя
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(
+        unique=True
+    )
+
+    phone = models.CharField(max_length=35, blank=True, null=True)
+    tg_nick = models.CharField(max_length=50, blank=True, null=True)
+    avatar = models.ImageField(upload_to="users/avatars", blank=True, null=True)
+
+    USERNAME_FIELD = "email"  # Указываем, что для аутентификации используется email
+    REQUIRED_FIELDS = (
+        []
+    )
+
+    def __str__(self):
+        return self.email

1d5b87c3ecba696554346d04dac2f428d71a5a85
Liudmila Beloritskaya <meiramirth@gmail.com>
2025-01-15 08:44:37 +0200

    add admin cod in present and users

--- present/admin.py
+++ present/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "category", "in_stock")
+    search_fields = ("name", "category__name")
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
